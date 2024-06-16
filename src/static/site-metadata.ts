2a19ba002882a2d484e50bd5b528042a97f9fabc
cuijianzhuang <cui2421011864@gmail.com>
2024-06-16 22:13:32 +0800

    添加个人信息

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -13,7 +13,7 @@ interface ISiteMetadataResult {
 const data: ISiteMetadataResult = {
   siteTitle: 'Workouts Map',
   siteUrl: 'https://run.cuijianzhuang.com',
-  logo: 'https://avatars.githubusercontent.com/u/31424401?v=4',
+  logo: 'https://raw.githubusercontent.com/cuijianzhuang/picx-images-hosting/master/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20240616220617.7p3gjjhany.webp',
   description: 'Personal site and blog',
   keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
   navLinks: [
